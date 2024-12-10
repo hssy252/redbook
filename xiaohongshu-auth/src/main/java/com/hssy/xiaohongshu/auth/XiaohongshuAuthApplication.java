e289cb20004055f4d6dacd8e4920db237043064e
hssy252 <1375925103@qq.com>
2024-12-10 13:59:18 +0800

    认证服务整合mybatis

--- xiaohongshu-auth/src/main/java/com/hssy/xiaohongshu/auth/XiaohongshuAuthApplication.java
+++ xiaohongshu-auth/src/main/java/com/hssy/xiaohongshu/auth/XiaohongshuAuthApplication.java
@@ -1,9 +1,11 @@
 package com.hssy.xiaohongshu.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hssy.xiaohongshu.auth.domain.mapper")
 public class XiaohongshuAuthApplication {
 
     public static void main(String[] args) {
