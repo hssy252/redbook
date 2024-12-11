c2c2c1f85e513470a99ad0b4d344a7702488d447
hssy252 <1375925103@qq.com>
2024-12-11 21:45:45 +0800

    Mybatis生成Role相关代码

---
+++ xiaohongshu-auth/src/main/java/com/hssy/xiaohongshu/auth/domain/dataobject/RoleDO.java
@@ -0,0 +1,36 @@
+package com.hssy.xiaohongshu.auth.domain.dataobject;
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * @author 13759
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleDO {
+    private Long id;
+
+    private String roleName;
+
+    private String roleKey;
+
+    private Byte status;
+
+    private Integer sort;
+
+    private String remark;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Boolean isDeleted;
+
+}
