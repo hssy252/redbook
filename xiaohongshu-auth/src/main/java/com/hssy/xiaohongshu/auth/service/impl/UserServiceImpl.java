0beb52ab1616a24d496c2316cf482d766920f103
hssy252 <1375925103@qq.com>
2024-12-12 11:35:14 +0800

    优化参数错误校验逻辑（使用PreConditions和全局异常处理器）

--- xiaohongshu-auth/src/main/java/com/hssy/xiaohongshu/auth/service/impl/UserServiceImpl.java
+++ xiaohongshu-auth/src/main/java/com/hssy/xiaohongshu/auth/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hssy.xiaohongshu.auth.service.impl;
 
 import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.hssy.framework.commom.enums.DeletedEnum;
 import com.hssy.framework.commom.enums.StatusEnum;
@@ -67,9 +68,7 @@ public class UserServiceImpl implements UserService {
                 String verificationCode = userLoginReqVO.getCode();
 
                 // 校验入参验证码是否为空
-                if (StringUtils.isBlank(verificationCode)) {
-                    return Response.fail(ResponseCodeEnum.PARAM_NOT_VALID.getErrorCode(), "验证码不能为空");
-                }
+                Preconditions.checkArgument(StringUtils.isNotBlank(verificationCode), "验证码不能为空");
 
                 // 构建验证码 Redis Key
                 String key = RedisKeyConstants.buildVerificationCodeKey(phone);
