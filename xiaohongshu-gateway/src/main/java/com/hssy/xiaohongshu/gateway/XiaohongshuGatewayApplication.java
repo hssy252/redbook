87a5f7312bd1d4ebfd85cd9de26c5d5e1a6a72d8
hssy252 <1375925103@qq.com>
2024-12-13 21:27:31 +0800

    初始化网关服务

---
+++ xiaohongshu-gateway/src/main/java/com/hssy/xiaohongshu/gateway/XiaohongshuGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.hssy.xiaohongshu.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 13759
+ */
+
+@SpringBootApplication
+public class XiaohongshuGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuGatewayApplication.class, args);
+    }
+
+}
