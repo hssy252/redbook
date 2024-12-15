2de747daee481b34c2d3070cdc3bac3ea6639603
hssy252 <1375925103@qq.com>
2024-12-15 17:23:57 +0800

    搭建对象存储微服务

---
+++ xiaohongshu-oss/xiaohongshu-oss-biz/src/main/java/com/hssy/xiaohongshu/oss/biz/XiaohongshuOssBizApplication.java
@@ -0,0 +1,21 @@
+package com.hssy.xiaohongshu.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能简述
+ *
+ * @author hssy
+ * @version 1.0
+ * @since 2024/12/15 17:21
+ */
+
+@SpringBootApplication
+public class XiaohongshuOssBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuOssBizApplication.class, args);
+    }
+
+}
