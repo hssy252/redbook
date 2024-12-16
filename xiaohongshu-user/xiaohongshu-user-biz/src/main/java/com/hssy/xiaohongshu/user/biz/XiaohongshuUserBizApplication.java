3953179b518bfa53039844eca7c8135db5ad38f2
hssy252 <1375925103@qq.com>
2024-12-16 20:57:03 +0800

    用户微服务初始化（rpc和biz）

---
+++ xiaohongshu-user/xiaohongshu-user-biz/src/main/java/com/hssy/xiaohongshu/user/biz/XiaohongshuUserBizApplication.java
@@ -0,0 +1,20 @@
+package com.hssy.xiaohongshu.user.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能简述
+ *
+ * @author hssy
+ * @version 1.0
+ * @since 2024/12/16 20:51
+ */
+@SpringBootApplication
+public class XiaohongshuUserBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuUserBizApplication.class,args);
+    }
+
+}
