3b226fa0436729edcd3b199ce7c51a2cad2dbaef
hssy252 <1375925103@qq.com>
2024-12-16 21:30:50 +0800

    用户微服务配置数据库功能

--- xiaohongshu-user/xiaohongshu-user-biz/src/main/java/com/hssy/xiaohongshu/user/biz/XiaohongshuUserBizApplication.java
+++ xiaohongshu-user/xiaohongshu-user-biz/src/main/java/com/hssy/xiaohongshu/user/biz/XiaohongshuUserBizApplication.java
@@ -1,5 +1,6 @@
 package com.hssy.xiaohongshu.user.biz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2024/12/16 20:51
  */
 @SpringBootApplication
+@MapperScan("com.hssy.xiaohongshu.user.biz.domain.mapper")
 public class XiaohongshuUserBizApplication {
 
     public static void main(String[] args) {
