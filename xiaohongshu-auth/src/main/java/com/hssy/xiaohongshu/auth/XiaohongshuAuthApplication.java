032abc3596cda360107ea1ca96aed12b54b37274
hssy252 <1375925103@qq.com>
2024-12-18 11:25:33 +0800

    重构用户和认证微服务（通过Feign解耦），bugfix

--- xiaohongshu-auth/src/main/java/com/hssy/xiaohongshu/auth/XiaohongshuAuthApplication.java
+++ xiaohongshu-auth/src/main/java/com/hssy/xiaohongshu/auth/XiaohongshuAuthApplication.java
@@ -1,12 +1,11 @@
 package com.hssy.xiaohongshu.auth;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@MapperScan("com.hssy.xiaohongshu.auth.domain.mapper")
 @EnableFeignClients(basePackages = "com.hssy.xiaohongshu")
 public class XiaohongshuAuthApplication {
 
