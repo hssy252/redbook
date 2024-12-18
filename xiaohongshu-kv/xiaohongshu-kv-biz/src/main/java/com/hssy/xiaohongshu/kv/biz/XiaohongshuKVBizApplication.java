341ba561aa4ce29e76be877adb4a40e2776bce25
hssy252 <1375925103@qq.com>
2024-12-18 16:27:29 +0800

    初始化KV键值存储服务,用于短文本存储（Cassandra）

---
+++ xiaohongshu-kv/xiaohongshu-kv-biz/src/main/java/com/hssy/xiaohongshu/kv/biz/XiaohongshuKVBizApplication.java
@@ -0,0 +1,19 @@
+package com.hssy.xiaohongshu.kv.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能简述
+ *
+ * @author hssy
+ * @version 1.0
+ * @since 2024/12/18 16:05
+ */
+@SpringBootApplication
+public class XiaohongshuKVBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuKVBizApplication.class,args);
+    }
+}
