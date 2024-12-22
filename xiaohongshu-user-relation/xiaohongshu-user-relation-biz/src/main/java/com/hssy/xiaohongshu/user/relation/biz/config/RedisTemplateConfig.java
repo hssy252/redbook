ca8bc27ccf6bc286cf42f11be7d93b89a6eadc43
hssy252 <1375925103@qq.com>
2024-12-22 18:40:09 +0800

    完善用户关系服务基础配置

---
+++ xiaohongshu-user-relation/xiaohongshu-user-relation-biz/src/main/java/com/hssy/xiaohongshu/user/relation/biz/config/RedisTemplateConfig.java
@@ -0,0 +1,37 @@
+package com.hssy.xiaohongshu.user.relation.biz.config;
+
+import org.springframework.boot.autoconfigure.cache.CacheProperties.Redis;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * 功能简述
+ *
+ * @author hssy
+ * @version 1.0
+ * @since 2024/12/22 18:36
+ */
+@Configuration
+public class RedisTemplateConfig {
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+}
