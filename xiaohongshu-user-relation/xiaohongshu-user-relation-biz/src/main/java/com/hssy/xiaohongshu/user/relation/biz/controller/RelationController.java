4c3dacab513d62e34deccface88ed1b8515627a0
hssy252 <1375925103@qq.com>
2024-12-24 09:59:41 +0800

    初步实现取关接口

--- xiaohongshu-user-relation/xiaohongshu-user-relation-biz/src/main/java/com/hssy/xiaohongshu/user/relation/biz/controller/RelationController.java
+++ xiaohongshu-user-relation/xiaohongshu-user-relation-biz/src/main/java/com/hssy/xiaohongshu/user/relation/biz/controller/RelationController.java
@@ -3,6 +3,7 @@ package com.hssy.xiaohongshu.user.relation.biz.controller;
 import com.hssy.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.hssy.framework.commom.response.Response;
 import com.hssy.xiaohongshu.user.relation.biz.model.vo.FollowUserReqVO;
+import com.hssy.xiaohongshu.user.relation.biz.model.vo.UnfollowUserReqVO;
 import com.hssy.xiaohongshu.user.relation.biz.service.RelationService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +25,12 @@ public class RelationController {
     @ApiOperationLog(description = "关注用户")
     public Response<?> follow(@Validated @RequestBody FollowUserReqVO followUserReqVO) {
         return relationService.followUser(followUserReqVO);
+    }
+
+    @PostMapping("/unfollow")
+    @ApiOperationLog(description = "取关用户")
+    public Response<?> unfollow(@Validated @RequestBody UnfollowUserReqVO unfollowUserReqVO){
+        return relationService.unfollow(unfollowUserReqVO);
     }
 
 }
