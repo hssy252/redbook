15e58c362ccb0700fe5117bb13d41d63638ef1ae
hssy252 <1375925103@qq.com>
2024-12-28 19:46:39 +0800

    计数消费者整合快手buffertrigger实现流量聚合

--- xiaohongshu-count/xiaohongshu-count-biz/src/main/java/com/hssy/xiaohongshu/count/biz/consumer/CountFansConsumer.java
+++ xiaohongshu-count/xiaohongshu-count-biz/src/main/java/com/hssy/xiaohongshu/count/biz/consumer/CountFansConsumer.java
@@ -1,6 +1,10 @@
 package com.hssy.xiaohongshu.count.biz.consumer;
 
+import com.github.phantomthief.collection.BufferTrigger;
+import com.hssy.framework.commom.util.JsonUtils;
 import com.hssy.xiaohongshu.count.biz.constants.MQConstants;
+import java.time.Duration;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
@@ -21,8 +25,21 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class CountFansConsumer implements RocketMQListener<String> {
 
+    private final BufferTrigger<String> bufferTrigger = BufferTrigger.<String>batchBlocking()
+        .bufferSize(50000) // 缓存队列的最大容量
+        .batchSize(1000)   // 一批次最多聚合 1000 条
+        .linger(Duration.ofSeconds(1)) // 多久聚合一次
+        .setConsumerEx(this::consumeMessage)
+        .build();
+
     @Override
     public void onMessage(String body) {
-        log.info("## 消费到了 MQ 【计数: 粉丝数】, {}...", body);
+        // 往 bufferTrigger 中添加元素
+        bufferTrigger.enqueue(body);
+    }
+
+    private void consumeMessage(List<String> bodys) {
+        log.info("==> 聚合消息, size: {}", bodys.size());
+        log.info("==> 聚合消息, {}", JsonUtils.toJsonString(bodys));
     }
 }
