61ae6e983c31dd343adcefb232562fb1517dfddb
hssy252 <1375925103@qq.com>
2025-01-01 11:03:27 +0800

    搭建数据对齐模块

---
+++ xiaohongshu-data-align/src/main/java/com/hssy/xiaohongshu/data/align/XiaohongshuDataAlignApplication.java
@@ -0,0 +1,21 @@
+package com.hssy.xiaohongshu.data.align;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能简述
+ *
+ * @author hssy
+ * @version 1.0
+ * @since 2025/1/1 10:57
+ */
+
+@SpringBootApplication
+public class XiaohongshuDataAlignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuDataAlignApplication.class,args);
+    }
+
+}
